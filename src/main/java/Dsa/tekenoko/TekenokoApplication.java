1c0ccaacc432f55901e5b01a986f1f34b4eb82a7
krgrocon <krgrocon@gmail.com>
2024-06-21 19:45:26 +0900

    first-commit

---
+++ src/main/java/Dsa/tekenoko/TekenokoApplication.java
@@ -0,0 +1,13 @@
+package Dsa.tekenoko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TekenokoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TekenokoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Dsa/tekenoko/TekenokoApplicationTests.java
@@ -0,0 +1,13 @@
+package Dsa.tekenoko;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TekenokoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
